bc27a0b8b4b036248a09e8141a643d63424fbc5d
Clickybunty <90314828+Clickybunty@users.noreply.github.com>
2024-07-15 12:20:54 +0200

    PythonTS

---
+++ WelloWorld.py
@@ -0,0 +1,8 @@
+def begrueßung():    
+    print("Hallo " + name)
+
+name=input("Dein Name ist? ")
+begrueßung()
+
+eingabe=input("Gib irgend etwas ein")
+print("Die eingebene Variable ist ein: ", type(eingabe))
