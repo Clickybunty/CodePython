c06d5c81f01decf7be78a2adde744cef2052fd1c
Clickybunty <90314828+Clickybunty@users.noreply.github.com>
2024-07-16 14:36:05 +0200

    quiz  and function

--- WelloWorld.py
+++ WelloWorld.py
@@ -1,8 +1,25 @@
-def begrueßung():    
-    print("Hallo " + name)
+def quiz():    
+    Frage = input(Name + " Wie lautet die UID des Benutzers root? 1, -1, 255, 0, 65536 ")
+    
+    try:
+        Frage = int(Frage)    
+    except ValueError:
+        print(Name + " versuche es noch einmal!")
+        
+    if Frage == 0:
+        print("Richtig")
+    elif Frage!=0:
+        print("Falsch")
+        print("Falsch")
+        print("Super Falsch")
+        
+Name=input("Wie lautet dein Name? ")
+print("Du behauptest also, dass dein Name " + Name + " lautet")
+QuizStarten=input("Möchtest Du " + Name + " eine Quizfrage beantworten, dann schreibe Ja: ")
 
-name=input("Dein Name ist? ")
-begrueßung()
-
-eingabe=input("Gib irgend etwas ein")
-print("Die eingebene Variable ist ein: ", type(eingabe))
+if QuizStarten.lower()!="ja":
+    print("Schade dass Du nicht am Quiz teil nimmst. Auf wiedersehen " + Name)
+elif QuizStarten.lower()=="ja":
+    quiz()
+else:
+    print("Du hast nicht Ja geschrieben. Auf wiedersehen " + Name)
