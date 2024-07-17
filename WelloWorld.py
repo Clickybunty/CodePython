9dc46ee700adf67df91d1c66217590fee3ca7c80
Clickybunty <90314828+Clickybunty@users.noreply.github.com>
2024-07-17 14:11:00 +0200

    matplotlib trubble

--- WelloWorld.py
+++ WelloWorld.py
@@ -1,45 +1,10 @@
-import random
+import matplotlib.pyplot
 
-def RandomFunc():
-    RandNumber=(random.randint(1, 10))
-    return RandNumber
-i=0
-while i<10:
-    print(RandomFunc())
-    i=i+1
-    
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
 
-# Function
-name=input("Hey DU.. wie heißt Du? ")
-
-def Modulo(Modu):
-    return Modu % 2 == 0
-
-def Begrueßung():    
-    print(f"Hallo, {name}! Willkommen zum Python unterricht")
-
-Begrueßung()
-
-def ImQuadrat(Argument):
-    return Argument * Argument
-
-Input=int(input(name + " bitte gib eine Zahl ein um dessen Quadrat zu erhalten: "))
-
-if Input >= 0:    
-    Ergebnis=ImQuadrat(Input)
-    print("Das Quadrat von "+ str(Input) + " ist: " + str(Ergebnis))
-else:
-    print("Die Zahl ist kleiner oder gleich Null")
-
-InputPruefung = Modulo(Input)
-if InputPruefung:
-    print("Der Input Wert " + str(Input) +" ist gerade und")
-else:
-    print("Der Input Wert " + str(Input) +" ist ungrade und")
-    
-QuadratPruefung = Modulo(Ergebnis)
-if  QuadratPruefung:
-    print("Das Quadrat " + str(Ergebnis) +" ist gerade") 
-else:
-    print("Das Quadrat " + str(Ergebnis) +" ist ungrade")
-    
+matplotlib.pyplot.plot(x, y)
+matplotlib.pyplot.xlabel('x-Achse')
+matplotlib.pyplot.ylabel('Y-Achse')
+matplotlib.pyplot.title('Einfaches Liniendiagram')
+matplotlib.pyplot.show()
