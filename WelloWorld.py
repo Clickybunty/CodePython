1a202715dfbbd7af3102dabe560517d26907637a
Clickybunty <90314828+Clickybunty@users.noreply.github.com>
2024-07-17 12:20:07 +0200

    Gruppenarbeit

--- WelloWorld.py
+++ WelloWorld.py
@@ -1,53 +1,34 @@
-import tkinter as tk
+# Function
+name=input("Hey DU.. wie heißt Du? ")
 
-root = tk.Tk()
+def Modulo(Modu):
+    return Modu % 2 == 0
 
-# Benennt die Kopfzeile des GUI
-root.title("Haed vom Fenster")
+def Begrueßung():    
+    print(f"Hallo, {name}! Willkommen zum Python unterricht")
 
-# Definiert die Größe der GUI
-root.geometry("400x400")
+Begrueßung()
 
-# Beschränkt die Kleinste GUI Darstellung
-root.minsize(width=200, height=200)
+def ImQuadrat(Argument):
+    return Argument * Argument
 
-# Beschränkt die maximale GUI Darstellung
-root.maxsize(width=800, height=800)
+Input=int(input(name + " bitte gib eine Zahl ein um dessen Quadrat zu erhalten: "))
 
-# Ein Label innerhalb des GUI
-Label = tk.Label(root, text="Hallo Welt")
-Label.pack()
-
-# mainloop startet eine endlos schleife. 
-# Code darunter kann erst mal nicht ausgeführt werden
-# bis der mainloop beendet wird
-root.mainloop()
-
-
-
-
-def quiz():    
-    Frage = input(Name + " Wie lautet die UID des Benutzers root? 1, -1, 255, 0, 65536 ")
-    
-    try:
-        Frage = int(Frage)    
-    except ValueError:
-        print(Name + " versuche es noch einmal!")
-        
-    if Frage == 0:
-        print("Richtig")
-    elif Frage!=0:
-        print("Falsch")
-        print("Falsch")
-        print("Super Falsch")
-        
-Name=input("Wie lautet dein Name? ")
-print("Du behauptest also, dass dein Name " + Name + " lautet")
-QuizStarten=input("Möchtest Du " + Name + " eine Quizfrage beantworten, dann schreibe Ja: ")
-
-if QuizStarten.lower()!="ja":
-    print("Schade dass Du nicht am Quiz teil nimmst. Auf wiedersehen " + Name)
-elif QuizStarten.lower()=="ja":
-    quiz()
+if Input >= 0:    
+    Ergebnis=ImQuadrat(Input)
+    print("Das Quadrat von "+ str(Input) + " ist: " + str(Ergebnis))
 else:
-    print("Du hast nicht Ja geschrieben. Auf wiedersehen " + Name)
+    print("Die Zahl ist kleiner oder gleich Null")
+
+InputPruefung = Modulo(Input)
+if InputPruefung:
+    print("Der Input Wert " + str(Input) +" ist gerade und")
+else:
+    print("Der Input Wert " + str(Input) +" ist ungrade und")
+    
+QuadratPruefung = Modulo(Ergebnis)
+if  QuadratPruefung:
+    print("Das Quadrat " + str(Ergebnis) +" ist gerade") 
+else:
+    print("Das Quadrat " + str(Ergebnis) +" ist ungrade")
+    
