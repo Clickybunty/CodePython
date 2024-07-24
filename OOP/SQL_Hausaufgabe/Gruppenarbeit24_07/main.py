da06172b694d2b9535820e62956dea8ffd02b19f
Clickybunty <90314828+Clickybunty@users.noreply.github.com>
2024-07-24 11:49:41 +0200

    main_DB

--- OOP/SQL_Hausaufgabe/Gruppenarbeit24_07/main.py
+++ OOP/SQL_Hausaufgabe/Gruppenarbeit24_07/main.py
@@ -5,6 +5,13 @@ from insert_data import insert_data
 def main(): 
     create_tables() 
     insert_data() 
-# 
+    
+# __name__ == "__main__": 
+# Dies ist der Fall, 
+# wenn das Skript direkt ausgeführt wird. 
+# Das bedeutet, 
+# dass das Skript nicht importiert wird, 
+# sondern direkt von der Kommandozeile oder 
+# einer IDE gestartet wird.
 if __name__ == "__main__": 
     main()
